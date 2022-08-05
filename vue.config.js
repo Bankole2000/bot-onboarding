eda89e01fe30a195473dd5332e0809b6b3f79846
Bankole Esan <techybanky@gmail.com>
2022-08-05 09:49:27 +0100

    changed to https

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  chainWebpack: (config) => {
-    config.plugin("html").tap((args) => {
-      args[0].title = "Lugah Bot Onboarding";
-      return args;
-    });
-  },
-  // publicPath: process.env.NODE_ENV === "production" ? "/bot-onboarding" : "/"
-  publicPath: process.env.NODE_ENV === "production" ? "/" : "/"
+  // chainWebpack: (config) => {
+  //   config.plugin("html").tap((args) => {
+  //     args[0].title = "Lugah Bot Onboarding";
+  //     return args;
+  //   });
+  // },
+  // // publicPath: process.env.NODE_ENV === "production" ? "/bot-onboarding" : "/"
+  // publicPath: process.env.NODE_ENV === "production" ? "/" : "/"
   // publicPath: '',
   // baseUrl: '/projects/vue/webpieces/dist/',
 };
